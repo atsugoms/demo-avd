4fbf023be80362fad1335903d2c4d531df7bfe8e
Akinari Tsugo <akinaritsugo@microsoft.com>
2026-06-17 15:33:26 +0900

    ネットワークのみ

--- infra/bastion.tf
+++ infra/bastion.tf
@@ -1,10 +1,10 @@
 resource "azurerm_bastion_host" "hub_bastion" {
-  name                = "${var.prj}-${var.env}-hub-bastion"
+  name                = "${var.prj}-${var.env}-bastion"
   location            = azurerm_resource_group.avd_rg.location
   resource_group_name = azurerm_resource_group.avd_rg.name
 
   sku                = "Developer"
-  virtual_network_id = azurerm_virtual_network.hub_vnet.id
+  virtual_network_id = azurerm_virtual_network.vnet.id
 
   tags = {
     project     = var.prj
